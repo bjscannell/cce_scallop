d035c0c1de400960c924c9222768f92d0ad64bcd
Brittney Scannell <brittney.scannell@stonybrook.edu>
2022-12-29 15:29:46 -0500

    adding an R file to format data

---
+++ data_format.R
@@ -0,0 +1,24 @@
+
+# Setting up dataframes ---------------------------------------------------
+
+library(tidyverse)
+library(pscl)
+library(lme4)
+library(DHARMa)
+library(lmtest)
+library(GLMMadaptive)
+#import data
+raw <- read.csv("ScallopDensities.csv")
+
+
+#clean data
+Scallop <- raw %>%
+  dplyr::select(date, year, season, embayment, site, trans = transect_repl, adults = ad_yr_rawcount, juveniles = juv_yr_density.1) %>%
+  filter(!is.na(adults)) %>%
+  mutate(adults = as.integer(adults), 
+         date = as.Date(date, format = "%d-%b-%y"),
+         year = year-2000,
+         period = 
+           case_when(
+             year <= 18 ~ "Before",
+             year > 18 ~ "After"))
