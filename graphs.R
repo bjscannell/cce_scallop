4ae02f684dc905846927f970fe838315bf34fe2a
Brittney Scannell <brittney.scannell@stonybrook.edu>
2022-12-29 18:49:32 -0500

    editing dumbell plots

--- graphs.R
+++ graphs.R
@@ -5,7 +5,7 @@ baycount <- Scallop %>%
   group_by(embayment, year, season) %>% 
   mutate(embay_count = sum(adults)) %>% 
   group_by(embayment, year, season) %>% 
-  slice(1)
+  slice(1) 
 
 
 period_labs <- list(
@@ -28,8 +28,11 @@ ggplot(baycount, aes(x = factor(season,levels = c("spr", "fall "), labels = c("S
         axis.title.y = element_text(vjust = +3),
         axis.title.x = element_text(vjust = +3),
         plot.margin = margin(0.7,0.7,0.7,0.7, "cm")) +
-  scale_fill_manual(values = c( "#f2bf3b","#588b8b"))
+  scale_fill_manual(values = c( "#f2bf3b","#588b8b")) 
 
+ggsave("plots/boxplot.png", dpi = 360, bg = "white")
+  
+  
 
 
 # Change in scallop dumbbell plot -----------------------------------------
@@ -46,7 +49,7 @@ db_wide <- baycount %>%
 
 
 ggplot(data = db_wide) +
-  geom_segment(aes(y=embayment, yend=embayment, x=0, xend=600), # plot background 
+  geom_segment(aes(y=embayment, yend=embayment, x=-150, xend=600), # plot background 
                color="#b2b2b2", size=0.15) +
   geom_dumbbell(aes(y=embayment, x=After, xend=Before), # dumbbells
                 size=0.8, color="#b2b2b2", size_x=3, size_xend = 3,
@@ -57,18 +60,19 @@ ggplot(data = db_wide) +
                color = "darkred") +
   geom_text(data=filter(db_wide, embayment == "SB"), # before label
             aes(x=Before, y=embayment, label="Before Collapse"),
-            color="lightblue", size=3, vjust=-1.5, fontface="bold") +
+            color="lightblue", size=3, vjust=-2.8, fontface="bold") +
   geom_text(data=filter(db_wide, embayment == "SB"), # after label
             aes(x=After, y=embayment, label="After Collapse"),
-            color="orange", size=3, vjust=-1.5, fontface="bold") +
+            color="orange", size=3, vjust= 3.6, fontface="bold") +
   geom_text(aes(x=After, y=embayment, label=After), # add after number
             color="orange", size=2.75, vjust=2.5) +
   geom_text(aes(x=Before, y=embayment, label=Before), # add before number
-            color="lightblue", size=2.75, vjust=2.5) +
-  geom_rect(aes(xmin=600, xmax= 700, ymin=-Inf, ymax=Inf), fill="grey") + # diff col
-  geom_text(aes(label=paste0(round(((Before-After)/Before)*100, 2), "%"), y=embayment, x=650), fontface="bold", size=3) + # add the diff numbers
+            color="lightblue", size=-2.75, vjust=2.5) +
+  geom_rect(aes(xmin=-125, xmax= -60, ymin=-Inf, ymax=Inf), fill="grey") + # diff col
+  geom_text(aes(label=paste0(round(((Before-After)/Before)*-100, 2), "%"), 
+                y=embayment, x=-95), fontface="bold", size=3) + # add the diff numbers
   geom_text(data=filter(db_wide, embayment=="SB"),
-            aes(x=650, y=embayment, label="Decrease"),
+            aes(x=-95, y=embayment, label="get rid of this text"),
             color="black", size=3.1, vjust=-2, fontface="bold") + # add the word difference 
   labs(x = "Scallop Count", y = "Embayment") +
   theme_bw() +
@@ -77,11 +81,10 @@ ggplot(data = db_wide) +
     panel.grid.minor=element_blank(),
     panel.border=element_blank(),
     axis.ticks=element_blank(),
-    axis.text.x=element_blank(),
+    #axis.text.x=element_blank(),
     plot.title=element_text(size = 16, face="bold"),
     plot.title.position = "plot",
     plot.subtitle=element_text(face="italic", size=12, margin=margin(b=12)),
     plot.caption=element_text(size=8, margin=margin(t=12), color="#7a7d7e")
-  )
-
+  ) + coord_flip()
 
--- population_count.R
+++ population_count.R
@@ -42,8 +42,3 @@ tukey.two.way
 
 
 
-
-
-
-
-
