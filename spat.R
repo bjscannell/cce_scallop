8023cc244f2db77cb548f46f06a4b7570ef48312
flynndelany <flynndelany@gmail.com>
2022-12-30 12:22:18 -0500

    Model inverse gaussian trial

--- spat.R
+++ spat.R
@@ -1,6 +1,11 @@
 ggplot(SpatMaster, aes(spat)) + geom_histogram()
 
+zero_rm <- SpatMaster %>%
+  filter(spat != 0) %>%
+  mutate(year = year - 2000)
 
+m1 <- glmmTMB(formula = spat ~ year, 
+    family = inverse.gaussian(link = "log"), data = zero_rm)
 
-glm(formula = spat ~ year, 
-    family = inverse.gaussian(link = "log"), data = SpatMaster)
+summary(m1)
+plot(simulateResiduals(m1))
